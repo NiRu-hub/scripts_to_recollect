4aad7e68ff12da5db3921105cfea8b3210e5bcaf
Ruthran <g.ruthran@gmail.com>
2020-04-28 14:20:20 +0530

    Commit Forest Drawings

---
+++ Forest Drawing/testing_turtle.py
@@ -0,0 +1,13 @@
+import turtle
+
+screen_area = turtle.Screen()
+
+screen_area.setup(width = 800, height=800)
+
+#turtleobj.color('red')
+#turtleobj.pensize(10)
+turtleobj.shape('turtle')
+
+turtleobj.getscreen().mainloop()
+
+
