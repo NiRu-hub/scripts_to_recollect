a4c73fea1c94b2f8cae238bea7554699817714a1
Ruthran <g.ruthran@gmail.com>
2020-04-28 14:09:25 +0530

    Nisha commit

---
+++ Car_2.py
@@ -0,0 +1,18 @@
+class Car:
+    pass
+
+ford = Car()
+honda = Car()
+audi = Car()
+
+ford.color = 'Red'
+ford.speed = 300
+
+honda.color = 'White'
+honda.speed = 310
+
+audi.color = 'white'
+audi.speed = 320
+
+print(ford.color)
+print(ford.speed)
